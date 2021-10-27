2fefcb2ba1f419387b230d74ddc5d4dfe8c0da2b
WangPinChieh <pp8101313@hotmail.com>
2021-10-27 13:42:39 +0800

    add ListFiles

--- Controllers/S3UploadController.cs
+++ Controllers/S3UploadController.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AWS_S3.Controllers
@@ -11,21 +13,29 @@ namespace AWS_S3.Controllers
     public class S3UploadController : ControllerBase
     {
         private readonly IAmazonS3 _amazonS3;
+        private string _bucketName = "mycoreapibucket";
 
         public S3UploadController(IAmazonS3 amazonS3)
         {
             _amazonS3 = amazonS3;
         }
         [HttpPost]
-        public async Task<IActionResult> Upload()
+        public async Task<IActionResult> Upload(IFormFile file)
         {
             await _amazonS3.PutObjectAsync(new PutObjectRequest
             {
-                InputStream = System.IO.File.Open(@"D:\Downloads\FusionCharts.jpg", FileMode.Open, FileAccess.Read),
-                BucketName = "mycoreapibucket",
-                Key = "1234.jpg"
+                InputStream = file.OpenReadStream(),
+                BucketName = _bucketName,
+                Key = file.FileName
             });
             return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListFiles()
+        {
+            var files = await _amazonS3.ListObjectsAsync(_bucketName, CancellationToken.None);
+            return Ok(files);
         }
     }
 }
